290c000d937c655aaa14be72a96a64f89df5cadc
Ivan Apos <aposivanrodger@gmail.com>
2023-10-10 17:21:50 +0800

    Api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BingoController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('auth', [AuthController::class, 'store']);
+Route::post('auth/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('user', UserController::class);
+    Route::post('bingo/start', [BingoController::class, 'start']);
+    Route::post('bingo/{bingo}/pick-a-ball', [BingoController::class, 'pick']);
+    Route::apiResource('bingo', BingoController::class);
 });
