f77004d42ebe40da36973d9f6439e8b8a5db023f
Ivan Apos <aposivanrodger@gmail.com>
2023-10-10 21:47:51 +0800

    Updated routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BingoController;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +19,10 @@ Route::post('auth', [AuthController::class, 'store']);
 Route::post('auth/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('user', UserController::class);
     Route::post('bingo/start', [BingoController::class, 'start']);
+    Route::post('bingo/{bingo}', [BingoController::class, 'show']);
     Route::post('bingo/{bingo}/pick-a-ball', [BingoController::class, 'pick']);
+    Route::post('bingo/{bingo}/mark/{letter}/{number}', [BingoController::class, 'mark']);
+    Route::post('bingo/{bingo}/verify', [BingoController::class, 'verify']);
     Route::apiResource('bingo', BingoController::class);
 });
